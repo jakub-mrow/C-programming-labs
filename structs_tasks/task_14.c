368b6a9de763d415cbebd035a8755db8c53b9f96
Jakub Mrowczynski <jakub@mrowczynski.net>
2021-05-04 19:47:24 +0200

    changes to task_14

--- structs_tasks/task_14.c
+++ structs_tasks/task_14.c
@@ -1,57 +1,86 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #define MAX 100
 
 struct computer{
-    char name[20];
-    char producent[20];
+    char *name;
+    char *producent;
     double price;
 };
 
-void add(struct computer *computers, int *count){
-    if (*count < MAX){
-        printf("Prosze podac nazwe komputera, nazwe producenta oraz cene: \n");
-        scanf("%s%s%lf", &computers[*count].name, &computers[*count].producent, &computers[*count].price);
-        *count += 1;
-    }
-}
-void print_computers(struct computer *computers, int  *count){
+void add(struct computer **computers, int *count){
     int i;
-    for(i=0; i<*count; i++){
-        printf("Nazwa: %s\n", computers[i].name);
-        printf("Producent: %s\n", computers[i].producent);
-        printf("Cena: %lf\n", computers[i].price);
+    for(i = 0;i < MAX; i++){
+        if (computers[i] == NULL){
+            computers[i] = (struct computer*)malloc(sizeof(struct computer));
+            char a[30], a2[30];
+            printf("Prosze podac nazwe komputera, nazwe producenta oraz cene: \n");
+            scanf("%s%s%lf", a, a2, &computers[*count]->price);
+            computers[i]->name = (char*)malloc((strlen(a)+1)*sizeof(char));
+            computers[i]->producent = (char*)malloc((strlen(a2)+1)*sizeof(char));
+            strcpy(computers[i]->name, a);
+            strcpy(computers[i]->producent, a2);
+            *count += 1;
+            break;
+        }
     }
 }
-void search_computer(struct computer *computers, int *count, char name[20]){
+void print_computers(struct computer **computers, int  *count){
+    int i, j;
+    for(i = 0,j = 1;j <= *count && i < MAX; i++,j++){
+        if (computers[i] == NULL){
+            continue;
+        }
+        printf("%d. ------------------------------------\n", j);
+        printf("Nazwa: %s\n", computers[i]->name);
+        printf("Producent: %s\n", computers[i]->producent);
+        printf("Cena: %0.2lf\n", computers[i]->price);
+    }
+}
+void search_computer(struct computer **computers, int *count){
     int i, value;
+    char name[30];
+    printf("Jaki komputer wyszukac? \n");
+    scanf("%s", name);
     for (i=0;i <*count; i++){
-        value = strcmp(computers[i].name, name);
+        if (computers[i] == NULL){
+            continue;
+        }
+        value = strcmp(computers[i]->name, name);
         if (value == 0){
             printf("Znaleziono komputer: %s\n", name);
             break;
         }
     }
 }
-void delete(struct computer *computers, int*count, char name[20]){
+void delete(struct computer **computers, int*count){
     int i, value;
-    for (i=0; i<*count; i++){
-        value = strcmp(computers[i].name, name);
+    char name[30];
+    printf("Prosze podac nazwe komputera do usuniecia: \n");
+    scanf("%s", name);
+    for (i=0; i<MAX; i++){
+        if (computers[i] == NULL)
+            continue;
+        value = strcmp(computers[i]->name, name);
         if (value == 0){
-            
+            free(computers[i]);
+            computers[i] = NULL;
+            printf("\n");
+            *count -= 1;
+            break;
         }
     }
 }
-
-
-
 int main(){
-    struct computer computers[MAX];
-    int count = 0;
+    struct computer *computers[MAX];
+    int count = 0, i;
 
+    for (i=0; i<MAX; i++){
+        computers[i] = NULL;
+    }
     while(1){
         char c;
-        char name[20];
         printf("Prosze wybrac operacje: \n");
         printf("N | n  nowy komputer \nW | w  wyswietl wszystkie komputery \nS | s  szukaj komputera wedlug nazwy \nQ | q  koniec programu\nU | u  usun komputer wedlug nazwy \n");
         scanf("    %c", &c);
@@ -63,11 +92,10 @@ int main(){
             print_computers(computers, &count);
         }
         else if((c == 'S') || (c == 's')){
-            scanf("%s", name);
-            search_computer(computers, &count, name);
+            search_computer(computers, &count);
         }
         else if((c == 'U') || (c == 'u')){
-            
+            delete(computers, &count);
         }
         else if((c == 'Q') || (c == 'q'))
             break;     
