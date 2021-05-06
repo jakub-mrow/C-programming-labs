76a6c7a6007a23e4b7bbd781d34415cb8f3139fd
Jakub Mrowczynski <jakub@mrowczynski.net>
2021-05-06 21:22:27 +0200

    added files tasks

--- files_tasks/task_1.c
+++ files_tasks/task_1.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
 
 int main(){
+    int count = 0;
+    char c;
+    char check;
+    FILE *file;
 
+    printf("Jaki znak wyszukac? \n");
+    scanf("%c", &check);
+
+    file = fopen("text.txt", "r");
+
+    while ((c = fgetc(file)) != EOF){
+        if (check == c)
+            count++;
+    }
+    fclose(file);
+    printf("%d", count);
 
     return 0;
 }
