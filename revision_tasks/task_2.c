ca5b9b897729461af87d07b68f4cbb5c5d291a02
Jakub Mrowczynski <jakub@mrowczynski.net>
2021-05-11 21:39:01 +0200

    doing revision tasks

---
+++ revision_tasks/task_2.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+
+double calc(double **ret, int n, int m);
+void print_matrix(double **tab, int n, int m);
+
+int main(){
+    int n,m;
+    int i,j;
+    int row,col;
+    double **tab;
+    
+    int k, l;
+
+    printf("Prosze podac rozmiary macierzy: n x m\n");
+    scanf("%d%d", &n,&m);
+
+    double (*mat)[m] = allocate_matrix(n,m);
+
+    //alokowanie pamieci na macierz
+    tab = malloc(n * sizeof(*tab));
+    for(k = 0;k<n;k++){
+        tab[k] = malloc(m * sizeof(*tab[k]));
+    }
+
+    printf("Prosze podac elementy macierzy\n");
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            scanf("%lf", &tab[i][j]);
+        }
+    }
+
+    print_matrix(tab, n,m);
+
+    return 0;
+}
+
+void print_matrix(double **tab, int n, int m){
+    int i,j;
+
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            printf("%0.2lf ", tab[i][j]);
+        }
+        printf("\n");
+    }
+
+}
+double calc(double **tab, int n, int m){
+    double **ret;
+    int i,j,l;
+    double a;
+
+    ret = malloc(n * sizeof(*ret));
+    for(l=0;l<n;l++){
+        ret[l] = malloc(m * sizeof(*ret[l]));
+    }
+
+    for(i=0;i<n;i++){
+        for(j=0;j<m;j++){
+            a = tab[i][j];
+            if (a == -1)
+                ret[i][j] = 3.31;
+            else
+                ret[i][j] = (sin(a) * sin(a)-3)/(sqrt(a+1));
+        }
+    }
+
+    return ;
+}
+void *allocate_matrix(int nRow, int mCol){
+    return malloc(sizeof(double[nRow][mCol]));
+}
+
