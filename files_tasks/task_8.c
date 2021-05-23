2242191c2c765791cd16a37a07ac46abf98d3c84
Jakub Mrowczynski <jakub@mrowczynski.net>
2021-05-23 23:07:58 +0200

    finished logic to task_8

--- files_tasks/task_8.c
+++ files_tasks/task_8.c
@@ -4,34 +4,34 @@
 
 int main(){
     FILE *f;
-    f = fopen("input_task_8.txt", "r");
+    f = fopen("input_task_8.txt", "rt");
     if (f == NULL)
         return 0;
     
     int columns = 0;
     int lines = 0;
     int c;
-    
-    int n = 0,akt=0;
-    char r;
-    fscanf (f, "%d", &n);
-    while (fscanf(f,"%d",&n)==1){  
-        ++akt;
-        fscanf (f, "%c", &r);
-        if(r=='\0')
-            break;
-        if(r=='\n'){
+
+    int act = 0;
+    char ch;  
+    while(!feof(f)){
+        ch = fgetc(f);
+        if (ch == ' '){
+            ++act;
+            if (act > columns)
+                columns = act;
+        }
+        else if (ch == '\n'){
             ++lines;
-            if(akt>columns){
-                columns=akt;
-                akt=0;
-            }
-        }   
+            act = 0;
+        }
     }
-    
+    ++columns;
+    ++lines;
+
     printf("%d %d\n", columns, lines);
     int tab[lines][columns];
-
+    char check[lines][columns];
 
     for(int i = 0; i < lines; i++){
         for(int j = 0; j<columns; j++){
@@ -39,12 +39,20 @@ int main(){
         }
     }
 
+    for(int i = 0; i < lines; i++){
+        for(int j = 0; j<columns; j++){
+            check[i][j] = 'u';
+        }
+    }
+
     fseek(f, 0, SEEK_SET);
     int x;
     int i = 0, j = 0;
     char t;
+    
     while(fscanf(f,"%d", &x)==1){
         tab[i][j] = x;
+        check[i][j] = 'x';
         fscanf(f, "%c", &t);
         if (t==' ')
             ++j;
@@ -62,27 +70,40 @@ int main(){
         }
         printf("\n");
     }
+
+    for(int i = 0; i < lines; i++){
+        for(int j = 0; j<columns; j++){
+            printf("%c ", check[i][j]);
+        }
+        printf("\n");
+    }
+
     printf("---------- Suma -----------\n");
     int sum;
-    for(int i = 0; i < lines; i++){
+    for(int i = 0; i < columns; i++){
         sum = 0;
-        for(int j = 0; j < columns; j++){
-            sum = sum + tab[j][i];
+        for(int j = 0; j < lines; j++){
+            if (check[j][i] == 'x')
+                sum = sum + tab[j][i];
         }
         printf("%d ", sum);
     }
     printf("\n");
-    printf("---------- Srednia -----------\n");
+    printf("--------- Srednia ---------\n");
     double sum_av;
-    for(int i = 0; i < lines; i++){
+    int count_lines;
+    for(int i = 0; i < columns; i++){
         sum_av = 0;
-        for(int j = 0; j < columns; j++){
-            sum_av = sum_av + tab[j][i];
+        for(int j = 0; j < lines; j++){
+            if (check[j][i] == 'x'){
+                ++count_lines;
+                sum_av = sum_av + tab[j][i];
+            }
         }
-        printf("%lf ", sum_av/lines);
+        printf("%lf ", sum_av/count_lines);
+        count_lines = 0;
     }
-
-
+    
     fclose(f);
     return 0;
 }
